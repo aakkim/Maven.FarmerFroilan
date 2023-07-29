19ab01a002d27a1350b98c344522bbdb6f856f91
Angela Kim <aangelakkim@gmail.com>
2023-07-29 17:26:09 -0400

    tests for croprow

--- src/main/java/com/zipcodewilmington/froilansfarm/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropRow.java
@@ -1,12 +1,20 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.Food.Corn;
 import com.zipcodewilmington.froilansfarm.Food.Crop;
+import com.zipcodewilmington.froilansfarm.Food.Tomato;
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
 
 import java.util.ArrayList;
 
 public class CropRow <CropType extends Crop>extends ArrayList<CropType> {
     ArrayList crops;
+
     public CropRow() {
         crops = new ArrayList();
+    }
+
+    public boolean fertilize(Edible edible) {
+        return edible instanceof Corn || edible instanceof Tomato;
     }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
@@ -1,7 +1,63 @@
 package com.zipcodewilmington.froilansfarm;
 
+
+import com.zipcodewilmington.froilansfarm.Food.Corn;
+import com.zipcodewilmington.froilansfarm.Food.Egg;
+import com.zipcodewilmington.froilansfarm.Food.Tomato;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CropRowTest {
+public class CropRowTest {
+    CropRow cropRow = new CropRow();
+
+    @Test
+    public void testNullaryConstructor() {
+
+        Assert.assertTrue(cropRow.isEmpty());
+    }
+
+    @Test
+    public void testNullaryConstructor2() {
+
+        Assert.assertNotNull(cropRow);
+    }
+
+    @Test
+    public void testCropsList() {
+        cropRow.add(new Corn());
+        cropRow.add(new Tomato());
+        int expected = 2;
+
+        int actual = cropRow.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCropsList2() {
+        cropRow.add(new Corn());
+        cropRow.add(new Tomato());
+        cropRow.add(new Tomato());
+        cropRow.add(new Tomato());
+        int expected = 3;
+
+        int actual = cropRow.size();
+
+        Assert.assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void testFertilize() {
+        Corn corn = new Corn();
+        Assert.assertTrue(cropRow.fertilize(corn));
+    }
+
+    @Test
+    public void testFertilize2() {
+        Egg egg = new Egg();
+        Assert.assertFalse(cropRow.fertilize(egg));
+    }
 
 }
